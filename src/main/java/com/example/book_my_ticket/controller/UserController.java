0bb13bb4cce72dcd4779fa95f79e31651ba27469
Dhanush980 <143881097+Dhanush980@users.noreply.github.com>
2025-11-25 12:52:44 +0530

    project creation is done

---
+++ src/main/java/com/example/book_my_ticket/controller/UserController.java
@@ -0,0 +1,5 @@
+package com.example.book_my_ticket.controller;
+
+public class UserController {
+
+}
---
+++ src/main/java/com/example/book_my_ticket/repository/UserRepository.java
@@ -0,0 +1,5 @@
+package com.example.book_my_ticket.repository;
+
+public class UserRepository {
+
+}
